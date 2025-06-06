ac6579fd517225ff55b35ab941bf9b8f5452c929
Prem Agarwal <prem.m.agarwal@gmail.com>
2025-06-06 11:13:58 -0700

    Create validParentheses.java

---
+++ validParentheses.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+            // Push opening brackets
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } 
+            // Handle closing brackets
+            else {
+                if (stack.isEmpty()) return false; // No match
+
+                char top = stack.pop();
+
+                if ((c == ')' && top != '(') ||
+                    (c == '}' && top != '{') ||
+                    (c == ']' && top != '[')) {
+                    return false;
+                }
+            }
+        }
+
+        // Stack should be empty if all brackets matched
+        return stack.isEmpty();
+    }
+}
