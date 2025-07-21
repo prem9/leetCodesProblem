9d6f07b337359e006b4e17afd6a7446a014bafb8
Prem Agarwal <prem.m.agarwal@gmail.com>
2025-07-21 12:45:53 -0700

    Create rotateArray.java

---
+++ top150Interview/rotateArray.java
@@ -0,0 +1,25 @@
+class Solution {
+    public void rotate(int[] nums, int k) {
+        int n = nums.length;
+        k = k % n;  // In case k > n
+
+        // Step 1: Reverse the whole array
+        reverse(nums, 0, n - 1);
+
+        // Step 2: Reverse the first k elements
+        reverse(nums, 0, k - 1);
+
+        // Step 3: Reverse the remaining elements
+        reverse(nums, k, n - 1);
+    }
+
+    private void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int tmp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = tmp;
+            start++;
+            end--;
+        }
+    }
+}
